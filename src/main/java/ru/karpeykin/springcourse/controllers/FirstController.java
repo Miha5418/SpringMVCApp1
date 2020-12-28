a44857708f32c67457dbfc7c21926323d27a7086
karpeykin <karpejkin2016@gmail.com>
2020-12-28 15:25:58 +0700

    Урок 19

--- src/main/java/ru/karpeykin/springcourse/controllers/FirstController.java
+++ src/main/java/ru/karpeykin/springcourse/controllers/FirstController.java
@@ -3,6 +3,9 @@ package ru.karpeykin.springcourse.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author karpeykin
@@ -13,7 +16,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class FirstController {
 
     @GetMapping("/hello")
-    public String hellopage(){
+    public String hellopage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname){
+
+        System.out.println("Hello " + name + " " + surname);
         return "first/hello";
     }
 
