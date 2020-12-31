66d8b40c0431be5cf8af35f57cab13468feaa02f
karpeykin <karpejkin2016@gmail.com>
2020-12-31 15:35:00 +0700

    Урок 22

--- src/main/java/ru/karpeykin/springcourse/controllers/PeopleController.java
+++ src/main/java/ru/karpeykin/springcourse/controllers/PeopleController.java
@@ -3,10 +3,9 @@ package ru.karpeykin.springcourse.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import ru.karpeykin.springcourse.dao.PersonDAO;
+import ru.karpeykin.springcourse.models.Person;
 
 /**
  * @author karpeykin
@@ -24,16 +23,23 @@ public class PeopleController {
 
     @GetMapping()
     public String index(Model model){
-        // Получим всех людей из DAO и передадим в представление
         model.addAttribute("people", personDAO.index());
         return "people/index";
     }
+
     @GetMapping("/{id}")
     public String show(@PathVariable("id") int id,
                        Model model){
-        //Получим одного ччеловека по его ID
-
         model.addAttribute("person", personDAO.show(id));
         return "people/show";
+    }
+    @GetMapping("/new")
+    public String newPerson (@ModelAttribute("person") Person person) {
+        return "/people/new";
+    }
+    @PostMapping
+    public String create(@ModelAttribute("person") Person person){
+        personDAO.save(person);
+        return "redirect:/people";
     }
 }
